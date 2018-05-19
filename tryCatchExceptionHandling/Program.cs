3f67475659fb18da3c02600da3286ee8d9b9f3ef
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-19 11:10:34 -0700

    commit all

--- tryCatchExceptionHandling/Program.cs
+++ tryCatchExceptionHandling/Program.cs
@@ -10,6 +10,44 @@ namespace tryCatchExceptionHandling
     {
         static void Main(string[] args)
         {
+            bool isValidNumber = false;
+            while (!isValidNumber)
+            {
+                Console.WriteLine("What is your age?");
+                try
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age <= 0)
+                    {
+                        throw new ArgumentException();
+                    }
+                    else
+                    {
+                        isValidNumber = true;
+                    }
+                    int birthYear = DateTime.Now.Year - age;
+                    Console.WriteLine(birthYear);
+                    Console.ReadLine();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Please enter a number greater than 0.");
+                    Console.ReadLine();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid age. Please try again.");
+                    Console.ReadLine();
+
+                }
+            }
+
+
+            
+           
+
+            
+
         }
     }
 }
